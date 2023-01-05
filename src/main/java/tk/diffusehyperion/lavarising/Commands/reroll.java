a88a995cd9464c4bacadf94d22437ee97e035ef9
DiffuseHyperion <xieyingjie08@gmail.com>
2023-01-05 17:17:37 +0800

    change indentation and add getRequiredPlayers method

--- src/main/java/tk/diffusehyperion/lavarising/Commands/reroll.java
+++ src/main/java/tk/diffusehyperion/lavarising/Commands/reroll.java
@@ -41,7 +41,7 @@ public class reroll implements CommandExecutor, Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         requiredplayers = BigDecimal.valueOf(config.getInt("game.pregame.rerolling.percentageToPass")).multiply(BigDecimal.valueOf(Bukkit.getOnlinePlayers().size())).divide(BigDecimal.valueOf(100), 0, RoundingMode.DOWN).intValue();
         if (requiredplayers == 0) {
-           requiredplayers = 1;
+            requiredplayers = 1;
         }
         if (state == PREGAME && sender instanceof Player && config.getBoolean("game.pregame.rerolling.enabled") && !agreedlist.contains(sender) && allowedtoreroll && !starting) {
             agreedlist.add((Player) sender);
@@ -78,40 +78,40 @@ public class reroll implements CommandExecutor, Listener {
                     BarColor.valueOf(config.getString("timers.pregame.rerolling.colour")),
                     BarStyle.valueOf(config.getString("timers.pregame.rerolling.style")),
                     new BukkitRunnable() {
-                @Override
-                public void run() {
-                    requiredplayers = BigDecimal.valueOf(config.getInt("game.pregame.rerolling.percentageToPass")).multiply(BigDecimal.valueOf(Bukkit.getOnlinePlayers().size())).divide(BigDecimal.valueOf(100), 0, RoundingMode.DOWN).intValue();
-                    if (requiredplayers == 0) {
-                        requiredplayers = 1;
-                    }
+                        @Override
+                        public void run() {
+                            requiredplayers = getRequiredPlayers();
 
-                    rerollBossbar2 = Bukkit.createBossBar(
-                            Objects.requireNonNull(config.getString("timers.pregame.enabled.name")).replace("%required%", String.valueOf(requiredplayers)),
-                            BarColor.valueOf(config.getString("timers.pregame.enabled.colour")),
-                            BarStyle.valueOf(config.getString("timers.pregame.enabled.style")),
-                            BarFlag.PLAY_BOSS_MUSIC);
-                    rerollBossbar2.setProgress(1);
-                    for (Player p : Bukkit.getOnlinePlayers()) {
-                        rerollBossbar2.addPlayer(p);
-                    }
-                    allowedtoreroll = true;
-                }
-            });
+                            rerollBossbar2 = Bukkit.createBossBar(
+                                    Objects.requireNonNull(config.getString("timers.pregame.enabled.name")).replace("%required%", String.valueOf(requiredplayers)),
+                                    BarColor.valueOf(config.getString("timers.pregame.enabled.colour")),
+                                    BarStyle.valueOf(config.getString("timers.pregame.enabled.style")),
+                                    BarFlag.PLAY_BOSS_MUSIC);
+                            rerollBossbar2.setProgress(1);
+                            for (Player p : Bukkit.getOnlinePlayers()) {
+                                rerollBossbar2.addPlayer(p);
+                            }
+                            allowedtoreroll = true;
+                        }
+                    });
             rerollBossbar1 = pair.getValue0();
             rerollTask1 = pair.getValue1();
             rerollBossbar1.addPlayer(event.getPlayer());
         } else if (state == PREGAME) {
             Player p = event.getPlayer();
             if (allowedtoreroll) {
-                requiredplayers = BigDecimal.valueOf(config.getInt("game.pregame.rerolling.percentageToPass")).multiply(BigDecimal.valueOf(Bukkit.getOnlinePlayers().size())).divide(BigDecimal.valueOf(100), 0, RoundingMode.DOWN).intValue();
-                if (requiredplayers == 0) {
-                    requiredplayers = 1;
-                }
+                requiredplayers = getRequiredPlayers();
+
                 rerollBossbar2.setTitle(Objects.requireNonNull(config.getString("timers.pregame.enabled.name")).replace("%required%", String.valueOf(requiredplayers)));
                 rerollBossbar2.addPlayer(p);
             } else {
                 rerollBossbar1.addPlayer(p);
             }
         }
+    }
+
+    private int getRequiredPlayers() {
+        int requiredPlayers = BigDecimal.valueOf(config.getInt("game.pregame.rerolling.percentageToPass")).multiply(BigDecimal.valueOf(Bukkit.getOnlinePlayers().size())).divide(BigDecimal.valueOf(100), 0, RoundingMode.DOWN).intValue();
+        return requiredPlayers == 0 ? 1 : requiredPlayers;
     }
 }
