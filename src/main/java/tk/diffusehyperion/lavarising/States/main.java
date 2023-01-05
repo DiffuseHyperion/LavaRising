13285e88810e19a2e8a9d8ef03fbc9fe64472318
DiffuseHyperion <xieyingjie08@gmail.com>
2023-01-05 20:13:12 +0800

    fix lava rising and threshold warning

--- src/main/java/tk/diffusehyperion/lavarising/States/main.java
+++ src/main/java/tk/diffusehyperion/lavarising/States/main.java
@@ -10,7 +10,6 @@ import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import tk.diffusehyperion.lavarising.LavaRising;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -18,15 +17,17 @@ import java.util.HashMap;
 import java.util.Objects;
 
 import static tk.diffusehyperion.lavarising.LavaRising.*;
+import static tk.diffusehyperion.lavarising.States.overtime.triggerOvertime;
 
 public class main {
 
     public static HashMap<Player, BossBar> mainBossbars = new HashMap<>();
     public static int lavaheight;
     public static double[] timer;
+    public static BukkitRunnable lavaRiser;
 
     public static void triggerMain(){
-        LavaRising.state = States.MAIN;
+        state = States.MAIN;
         gm.GamePlayer.playSoundToAll(Sound.ENTITY_WITHER_AMBIENT);
         world.setPVP(true);
         String version = Bukkit.getBukkitVersion();
@@ -39,32 +40,29 @@ public class main {
             lavaheight = 1;
         }
         timer = new double[]{0};
-        int coords = BigDecimal.valueOf(LavaRising.config.getInt("game.grace.finalbordersize")).divide(BigDecimal.valueOf(2), RoundingMode.UP).intValue();
-        BukkitRunnable lavariser = new BukkitRunnable() {
+        int coords = BigDecimal.valueOf(config.getInt("game.grace.finalBorderSize")).divide(BigDecimal.valueOf(2), RoundingMode.UP).intValue();
+        lavaRiser = new BukkitRunnable() {
             @Override
             public void run() {
-                if (timer[0] >= LavaRising.config.getInt("game.main.lavaInterval")) {
+                if (timer[0] >= config.getInt("game.main.lavaInterval")) {
                     gm.GameWorld.fillBlocks(new Location(world, -coords, lavaheight, -coords), new Location(world, coords, lavaheight, coords), Material.LAVA);
                     lavaheight++;
                     timer[0] = 0;
                 }
-                if (lavaheight >= LavaRising.config.getInt("game.overtime.threshold")) {
-                    overtime.triggerOvertime();
-                    this.cancel();
-                }
-                if (state == States.POST) {
+                if (lavaheight >= config.getInt("game.overtime.threshold")) {
+                    triggerOvertime();
                     this.cancel();
                 }
                 timer[0] = BigDecimal.valueOf(timer[0]).add(BigDecimal.valueOf(0.1)).doubleValue();
             }
         };
-        lavariser.runTaskTimer(LavaRising.plugin,0, 2);
+        lavaRiser.runTaskTimer(plugin,0, 2);
         for (Player p : Bukkit.getOnlinePlayers()) {
             mainTimer(p);
         }
-        if (LavaRising.config.getBoolean("game.overtime.warning.enabled")) {
+        if (config.getBoolean("game.overtime.warning.enabled")) {
             BossBar bossbar = gm.GamePlayer.timer(config.getInt("game.overtime.warning.time"),
-                    config.getString("timers.overtime.warning.name"),
+                    Objects.requireNonNull(config.getString("timers.overtime.warning.name")).replace("%threshold%", String.valueOf(config.getInt("game.overtime.threshold"))),
                     BarColor.valueOf(config.getString("timers.overtime.warning.colour")),
                     BarStyle.valueOf(config.getString("timers.overtime.warning.style"))).getValue0();
 
@@ -75,7 +73,7 @@ public class main {
     }
 
     public static void mainTimer(Player player) {
-        BossBar bossbar = Bukkit.createBossBar(LavaRising.config.getString("timers.main.name"),
+        BossBar bossbar = Bukkit.createBossBar(config.getString("timers.main.name"),
                 BarColor.valueOf(config.getString("timers.main.colour")),
                 BarStyle.valueOf(config.getString("timers.main.style")),
                 BarFlag.PLAY_BOSS_MUSIC);
@@ -84,14 +82,14 @@ public class main {
         BukkitRunnable task = new BukkitRunnable() {
             @Override
             public void run() {
-                bossbar.setTitle(Objects.requireNonNull(LavaRising.config.getString("timers.main.name")).replace("%distance%", String.valueOf(player.getLocation().getBlockY() - lavaheight)).replace("%level%", String.valueOf(lavaheight)));
-                bossbar.setProgress(BigDecimal.valueOf(timer[0]).divide(BigDecimal.valueOf(LavaRising.config.getInt("game.main.lavaInterval")), 2, RoundingMode.HALF_EVEN).doubleValue());
+                bossbar.setTitle(Objects.requireNonNull(config.getString("timers.main.name")).replace("%distance%", String.valueOf(player.getLocation().getBlockY() - lavaheight)).replace("%level%", String.valueOf(lavaheight)));
+                bossbar.setProgress(BigDecimal.valueOf(timer[0]).divide(BigDecimal.valueOf(config.getInt("game.main.lavaInterval")), 2, RoundingMode.HALF_EVEN).doubleValue());
                 if (state == States.OVERTIME || state == States.POST) {
                     bossbar.removeAll();
                     this.cancel();
                 }
             }
         };
-        task.runTaskTimer(LavaRising.plugin, 0, 2);
+        task.runTaskTimer(plugin, 0, 2);
     }
 }
--- src/main/java/tk/diffusehyperion/lavarising/States/post.java
+++ src/main/java/tk/diffusehyperion/lavarising/States/post.java
@@ -1,26 +1,27 @@
 package tk.diffusehyperion.lavarising.States;
 
-import org.bukkit.boss.BossBar;
-import tk.diffusehyperion.lavarising.LavaRising;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Objects;
 
-import static tk.diffusehyperion.lavarising.LavaRising.config;
-import static tk.diffusehyperion.lavarising.LavaRising.gm;
+import static tk.diffusehyperion.lavarising.LavaRising.*;
+import static tk.diffusehyperion.lavarising.States.main.lavaRiser;
 import static tk.diffusehyperion.lavarising.States.main.mainBossbars;
 
 public class post {
     public static void triggerPost(Player winner){
-        LavaRising.state = States.POST;
+        state = States.POST;
         gm.GamePlayer.playSoundToAll(Sound.UI_TOAST_CHALLENGE_COMPLETE);
+
+        lavaRiser.cancel();
 
         for (BossBar b : mainBossbars.values()) {
             b.removeAll();
@@ -30,8 +31,8 @@ public class post {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("%winner%", winner.getDisplayName());
 
-        BossBar bossbar = gm.GamePlayer.customTimer(LavaRising.config.getInt("game.post.duration"),
-                LavaRising.config.getString("timers.post.name"),
+        BossBar bossbar = gm.GamePlayer.customTimer(config.getInt("game.post.duration"),
+                config.getString("timers.post.name"),
                 BarColor.valueOf(config.getString("timers.post.colour")),
                 BarStyle.valueOf(config.getString("timers.post.style")),
                 hashMap,
@@ -39,7 +40,7 @@ public class post {
             @Override
             public void run() {
                 for (Player p : Bukkit.getOnlinePlayers()) {
-                    p.kickPlayer(LavaRising.config.getString("timers.post.kickMessage"));
+                    p.kickPlayer(config.getString("timers.post.kickMessage"));
                 }
                 gm.GameServer.restart();
             }
@@ -47,9 +48,9 @@ public class post {
         for (Player p : Bukkit.getOnlinePlayers()) {
             bossbar.addPlayer(p);
         }
-        if (Objects.equals(LavaRising.config.getString("game.post.creativeMode"), "winner")) {
+        if (Objects.equals(config.getString("game.post.creativeMode"), "winner")) {
             winner.setGameMode(GameMode.CREATIVE);
-        } else if (Objects.equals(LavaRising.config.getString("game.post.creativeMode"), "all")) {
+        } else if (Objects.equals(config.getString("game.post.creativeMode"), "all")) {
             for (Player p : Bukkit.getOnlinePlayers()) {
                 p.setGameMode(GameMode.CREATIVE);
             }
